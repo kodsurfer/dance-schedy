7fa9bc384979e22823703ea8fd1bb8c2f0c7f543
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-10-24 19:31:55 +0300

    wip

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ class Student:
         self.role = role
 
 
+class Class:
+    def __init__(self, name, room, capacity):
+        self.name = name
+        self.room = room
+        self.capacity = capacity
+        self.students = []
+        self.leaders = 0
+        self.followers = 0
+
+
 if __name__ == '__main__':
     main()
